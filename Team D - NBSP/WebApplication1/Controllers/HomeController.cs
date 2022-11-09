fff2fcf24f42e2e0a9dfc15baec1c5627ac2c9c4
SimYuanYee <s10223621@connect.np.edu.sg>
2022-11-09 12:15:57 +0800

    added views and models

--- Team D - NBSP/WebApplication1/Controllers/HomeController.cs
+++ Team D - NBSP/WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,31 @@ namespace WebApplication1.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult StaffLogin(IFormCollection formData)
+        {
+            // Read inputs from textboxes
+            // Email address converted to lowercase
+            string loginID = formData["txtLoginID"].ToString().ToLower();
+            string password = formData["txtPassword"].ToString();
+            if (loginID == "admin" && password == "NBSP1234")
+            {
+                // Store Login ID in session with the key “LoginID”
+                HttpContext.Session.SetString("LoginID", loginID);
+                // Store user role “Staff” as a string in session with the key “Role”
+                HttpContext.Session.SetString("Role", "Admin");
+                // Redirect user to the "StaffMain" view through an action
+                return RedirectToAction("AdminMain");
+            }
+            else
+            {
+                // Store an error message in TempData for display at the index view
+                TempData["Message"] = "Invalid Login Credentials!";
+
+                // Redirect user back to the index view through an action
+                return RedirectToAction("Index");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
