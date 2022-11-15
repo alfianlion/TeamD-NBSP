1f33a69a09453d446031917a82ad788ef0523201
nidhoggmega <s10222609@connect.np.edu.sg>
2022-11-15 13:06:39 +0800

    Modified DAL for Member

--- NBSP/DAL/MemberDAL.cs
+++ NBSP/DAL/MemberDAL.cs
@@ -29,6 +29,31 @@ namespace NBSP.DAL
             //Connection String read.
             conn = new SqlConnection(strConn);
         }
+        public int Add(Member member)
+        {
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify an INSERT SQL statement which will
+            //return the auto-generated StaffID after insertion
+            cmd.CommandText = @"INSERT INTO Member (Name, EmailAddr, PhoneNo, Pwd)
+                                OUTPUT INSERTED.MemberID
+                                VALUES(@name, @email, @contact, @pwd)";
+            //Define the parameters used in SQL statement, value for each parameter
+            //is retrieved from respective class's property.
+            cmd.Parameters.AddWithValue("@name", member.Name);
+            cmd.Parameters.AddWithValue("@email", member.EmailAddr);
+            cmd.Parameters.AddWithValue("@contact", member.PhoneNo);
+            cmd.Parameters.AddWithValue("@email", member.Pwd);
+            //A connection to database must be opened before any operations made.
+            conn.Open();
+            //ExecuteScalar is used to retrieve the auto-generated
+            //StaffID after executing the INSERT SQL statement
+            member.MemberID = (int)cmd.ExecuteScalar();
+            //A connection should be closed after operations.
+            conn.Close();
+            //Return id when no error occurs.
+            return member.MemberID;
+        }
 
     }
 }
