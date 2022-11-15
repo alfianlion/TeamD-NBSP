0737f34d0617d1d67b59eb94c571ce319a47bb32
admin <admin@PF2T6TFT>
2022-11-15 11:46:41 +0800

    Member Model

--- NBSP/Models/Member.cs
+++ NBSP/Models/Member.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,11 @@ namespace NBSP.Models
 {
     public class Member
     {
+        [Display(Name = "Member ID")] public int MemberID { get; set; }
+        [Display(Name = "Name")] [StringLength(50)] public string Name { get; set; }
+        [Display(Name = "Email Address")] [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]  [StringLength(255)] public string EmailAddr { get; set; }
+        [Display(Name = "Phone Number")] public int PhoneNo { get; set; }
+        [Display(Name = "Password")] [StringLength(255)] public string Pwd { get; set; }
+     
     }
 }
