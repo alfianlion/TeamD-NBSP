c880fc6a3c0d3415aea934f4da2d3bc33c1001b9
SimYuanYee <s10223621@connect.np.edu.sg>
2022-11-15 13:00:44 +0800

    member.cs

--- NBSP/Models/Member.cs
+++ NBSP/Models/Member.cs
@@ -9,11 +9,20 @@ namespace NBSP.Models
 {
     public class Member
     {
-        [Display(Name = "Member ID")] public int MemberID { get; set; }
-        [Display(Name = "Name")] [StringLength(50)] public string Name { get; set; }
-        [Display(Name = "Email Address")] [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]  [StringLength(255)] public string EmailAddr { get; set; }
-        [Display(Name = "Phone Number")] public int PhoneNo { get; set; }
-        [Display(Name = "Password")] [StringLength(255)] public string Pwd { get; set; }
+        [Display(Name = "Member ID")] 
+        public int MemberID { get; set; }
+        [Display(Name = "Name")] [StringLength(50)]
+        public string Name { get; set; }
+
+        [Display(Name = "Email Address")] 
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")] 
+        [StringLength(255)] public string EmailAddr { get; set; }
+        [Display(Name = "Phone Number")] 
+        public int PhoneNo { get; set; }
+
+        [Display(Name = "Password")] 
+        [StringLength(255)] 
+        public string Pwd { get; set; }
      
     }
 }
