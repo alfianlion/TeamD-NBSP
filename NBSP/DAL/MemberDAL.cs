4872c6a3c66eef9b372674fb5710c98d41265e36
nidhoggmega <s10222609@connect.np.edu.sg>
2022-11-15 13:18:19 +0800

    Debugging

--- NBSP/DAL/MemberDAL.cs
+++ NBSP/DAL/MemberDAL.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using NBSP.Models;
 using System.IO;
-using System.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
+
 
 namespace NBSP.DAL
 {
