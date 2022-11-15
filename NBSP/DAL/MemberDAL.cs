969521afc704b6faeac16ecf455c769a6142bfea
SimYuanYee <s10223621@connect.np.edu.sg>
2022-11-15 11:30:30 +0800

    sql

--- NBSP/DAL/MemberDAL.cs
+++ NBSP/DAL/MemberDAL.cs
@@ -5,13 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using NBSP.Models;
+using System.IO;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace NBSP.DAL
 {
     public class MemberDAL
     {
-        private IConfiguration Configuration { get; set; }
+        private IConfiguration Configuration { get; }
         private SqlConnection conn;
+        //Constructor
+        public MemberDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "NBSPConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
 
     }
 }
